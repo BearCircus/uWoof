2a829d865966ce75bb7062be3aa65b6ab475ae6b
Isaac Castillo <isaac.castilloherrera036@gmail.com>
2022-05-10 18:25:52 -0500

    mas cosas

--- public/js/pets.js
+++ public/js/pets.js
@@ -1,12 +1,3 @@
-const axios = require("axios");
-mainPage
-axios({
-  method: "get",
-  url: "http://localhost:3000/api/pets",
-}).then((res)=>{
-    const data=res.data;
-    console.log(data);
-})
-async function getProducts(){
+async function getPets() {
   const res = await fetch("http://localhost:3000");
 }
