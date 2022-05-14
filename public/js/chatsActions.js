97cfe6cd9e4b65f409878a6e9def20006dbf8b9e
Isaac Castillo <isaac.castilloherrera036@gmail.com>
2022-05-14 12:33:17 -0500

    fix profile picture

--- public/js/chatsActions.js
+++ public/js/chatsActions.js
@@ -1,164 +1,180 @@
 // Funcion para obtener los dos arreglos de los chats del usuario loggeado ya sea owner o guest
-async function getChats(query){
+async function getChats(query) {
+  let queryParams = query ? "?name=" + query : "";
 
-    let queryParams = query ? "?name=" + query : "";
+  const response = await fetch("/api/chat" + queryParams, {
+    method: "GET",
+    headers: {
+      "x-auth": sessionStorage.getItem("token"),
+    },
+  });
 
-    const response = await fetch('/api/chat' + queryParams, {
-        method: 'GET',
-        headers: {
-            "x-auth": sessionStorage.getItem("token")
-        }
-    });
-
-    const data = await response.json();
-    return data;
+  const data = await response.json();
+  return data;
 }
 
 //Funcion obtiene los arreglos de los chats, identifica donde iran y llama la funcion con la estructura html
-async function chatsToHtml(data){
-    console.log(data);
-    let divChats = document.querySelector("ul#chats");
-    console.log(divChats);
+async function chatsToHtml(data) {
+  console.log(data);
+  let divChats = document.querySelector("ul#chats");
+  console.log(divChats);
 
-    divChats.innerHTML = data.ownerChats.map(chat => chatToHTMLBanner(chat, true)).join("");
-    divChats.innerHTML += data.guestChats.map(chat => chatToHTMLBanner(chat, false)).join("");
+  divChats.innerHTML = data.ownerChats
+    .map((chat) => chatToHTMLBanner(chat, true))
+    .join("");
+  divChats.innerHTML += data.guestChats
+    .map((chat) => chatToHTMLBanner(chat, false))
+    .join("");
 }
 
 //Funcion que retorna el html que se necesita para que salgan los chats del usuario loggeado
-function chatToHTMLBanner(chat, owner){
-    return `
-    <li class="person ${owner ? 'owner' : 'guest'}" data-chat="person1" onclick="selectChat('${chat._id}', ${owner})"> 
+function chatToHTMLBanner(chat, owner) {
+  return `
+    <li class="person ${
+      owner ? "owner" : "guest"
+    }" data-chat="person1" onclick="selectChat('${chat._id}', ${owner})"> 
         <div class="user">
             <img src=${chat.idPost.image} alt="Retail Admin">
         </div>
 
         <p class="name-time">
-            <span class="name ${owner ? 'owner' : 'guest'}">${chat.idPost.name} ${chat.idPost.animal}</span>
+            <span class="name ${owner ? "owner" : "guest"}">${
+    chat.idPost.name
+  } ${chat.idPost.animal}</span>
             <br>
             <span class="time"> Owner: ${chat.idOwner.name}</span>
         </p>
     </li>
-`
+`;
 }
 
 //todo: guardar el chatId, encontrar el chat del que se quiere actuar y mandarlo
-function selectChat(chatId, owner){
-    console.log(chatId);
-    sessionStorage.setItem("chatId", chatId);
-    let selectedChat = "";
-    let chats = JSON.parse(sessionStorage.getItem("chats"));
+function selectChat(chatId, owner) {
+  console.log(chatId);
+  sessionStorage.setItem("chatId", chatId);
+  let selectedChat = "";
+  let chats = JSON.parse(sessionStorage.getItem("chats"));
 
-    if(owner){
-        selectedChat = chats.ownerChats.find(chat => chat._id == chatId);
-    }else{
-        selectedChat = chats.guestChats.find(chat => chat._id == chatId);
-    }
+  if (owner) {
+    selectedChat = chats.ownerChats.find((chat) => chat._id == chatId);
+  } else {
+    selectedChat = chats.guestChats.find((chat) => chat._id == chatId);
+  }
 
-    console.log(selectedChat);
-    let divMensajesChat = document.querySelector("#chatEspecifico");
-    divMensajesChat.innerHTML = specificChatHTML(selectedChat, owner);
+  console.log(selectedChat);
+  let divMensajesChat = document.querySelector("#chatEspecifico");
+  divMensajesChat.innerHTML = specificChatHTML(selectedChat, owner);
 }
 
 //Funcion que identifica el textarea donde el usuario escribiria el mensaje y realiza la actualizacion del chat con el nuevo mensaje
-async function sendNewMessage(){
+async function sendNewMessage() {
+  let message = {
+    msg: document.getElementById("inputMessage").value,
+  };
+  console.log(message);
 
-    let message = {
-        'msg': document.getElementById("inputMessage").value
-    }
-    console.log(message);
+  const resp = await fetch("/api/chat/" + sessionStorage.getItem("chatId"), {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      "x-auth": sessionStorage.getItem("token"),
+    },
+    body: JSON.stringify(message),
+  });
 
-    const resp = await fetch('/api/chat/' + sessionStorage.getItem("chatId"), {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-            "x-auth": sessionStorage.getItem("token")
-        },
-        body: JSON.stringify(message)
-    })
-
-    const result = await resp.json();
-    console.log(result);
-    window.setInterval(location.reload(), 500);
+  const result = await resp.json();
+  console.log(result);
+  window.setInterval(location.reload(), 500);
 }
 
-async function buscarChat(){
-    event.preventDefault();
-    let divBuscador = document.getElementById("busqueda").value;
-    console.log(divBuscador);
-    let data = await getChats(divBuscador);
-    chatsToHtml(data);
+async function buscarChat() {
+  event.preventDefault();
+  let divBuscador = document.getElementById("busqueda").value;
+  console.log(divBuscador);
+  let data = await getChats(divBuscador);
+  chatsToHtml(data);
 }
 
 //Funcion para organizar las funciones de chat
-async function start(){
-    let data = await getChats();
-    chatsToHtml(data);
-    sessionStorage.setItem("chats", JSON.stringify(data));
+async function start() {
+  let data = await getChats();
+  chatsToHtml(data);
+  sessionStorage.setItem("chats", JSON.stringify(data));
 }
 
 start();
 
 //Funcion para crear un chat si no existe al hacer clic a send message
-async function processToChat(post_id){
-    let ownerId = post_id.userID;
-    console.log(ownerId);
+async function processToChat(post_id) {
+  let ownerId = post_id.userID;
+  console.log(ownerId);
 
-    let receptor_id = await getUsersToFind(ownerId);
-    console.log(receptor_id);
+  let receptor_id = await getUsersToFind(ownerId);
+  console.log(receptor_id);
 
-    await fetch ('/api/chat/' + post_id + '/' + receptor_id, {
-        method: 'POST',
-        headers: {
-            "x-auth": sessionStorage.getItem("token"),
-            'Content-Type': 'application/json'
-        }
-    })
+  await fetch("/api/chat/" + post_id + "/" + receptor_id, {
+    method: "POST",
+    headers: {
+      "x-auth": sessionStorage.getItem("token"),
+      "Content-Type": "application/json",
+    },
+  });
 }
 
 //Funcion que obtiene todos los usuarios
-async function getUsersToFind(ownerId){
-    const response = await fetch('/api/register', {
-        method: 'GET',
-    });
+async function getUsersToFind(ownerId) {
+  const response = await fetch("/api/register", {
+    method: "GET",
+  });
 
-    const data = await response.json();
+  const data = await response.json();
 
-    let obj = data.find(idOwner => idOwner.id == ownerId);
-    return obj._id;
+  let obj = data.find((idOwner) => idOwner.id == ownerId);
+  return obj._id;
 }
 
 //Funcion que contiene la esctrucura de los chats con los mensajes (dentro del ul)
-function specificChatHTML(chat, owner){
-    let myId = owner ? chat.idOwner._id : chat.idGuest._id;
+function specificChatHTML(chat, owner) {
+  let myId = owner ? chat.idOwner._id : chat.idGuest._id;
 
-    return `
+  return `
     <div class="selected-user" id="msgTo">
-        <span><span class="name">${chat.idPost.name} Pet Conversation</span></span>
+        <span><span class="name">${
+          chat.idPost.name
+        } Pet Conversation</span></span>
     </div>
                                 
     <div class="chat-container">
         <ul class="chat-box chatContainerScroll" id="mensajes">
-        ${chat.messages.map(msg => msg_html(msg, msg.idSenderUser == myId ? "right" : "left", msg.idSenderUser == chat.idOwner._id ? chat.idOwner : chat.idGuest)).join('')}
+        ${chat.messages
+          .map((msg) =>
+            msg_html(
+              msg,
+              msg.idSenderUser == myId ? "right" : "left",
+              msg.idSenderUser == chat.idOwner._id ? chat.idOwner : chat.idGuest
+            )
+          )
+          .join("")}
         </ul>
         <div class="form-group mt-3 mb-0">
             <textarea class="form-control" rows="3" id="inputMessage" placeholder="Type your message here..."></textarea>
             <button type="button" class="btn btn-info" onclick="sendNewMessage()">send</button>
         </div>
     </div>
-    `
+    `;
 }
 
 //Funcion que contiene la forma del mensaje del otro usuario en html
-function msg_html(msg, side, user){
-    let url =
+function msg_html(msg, side, user) {
+  let url =
     "https://happytravel.viajes/wp-content/uploads/2020/04/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png";
 
-    if (user.image) {
-        url = user.image;
-    }
+  if (user.image) {
+    url = user.image;
+  }
 
-    if(side == "left"){
-        return `
+  if (side == "left") {
+    return `
             <li class="chat-${side}">
                 <div class="chat-avatar">
                     <img src=${url} alt="Retail Admin">
@@ -168,9 +184,9 @@ function msg_html(msg, side, user){
                 <div class="chat-text">${msg.message}</div>
                 <div class="chat-hour">${msg.date} <span class="fa fa-check-circle"></span></div>
             </li>
-    `
-    }else{
-        return `
+    `;
+  } else {
+    return `
             <li class="chat-${side}">
             <div class="chat-hour">${msg.date} <span class="fa fa-check-circle"></span></div>
                 <div class="chat-text">${msg.message}</div>
@@ -181,16 +197,15 @@ function msg_html(msg, side, user){
                 </div>
                 <button type="button" class="btn btn-outline-info" onclick="deleteMessage(${msg._id})">X</button>
             </li>
-    `
-    }
+    `;
+  }
 }
 
-async function deleteMessage(msg_id){
-    console.log(msg_id);
-    const resp = await fetch('api/chat/' + msg_id, {
-        method: 'DELETE'
-    })
+async function deleteMessage(msg_id) {
+  console.log(msg_id);
+  const resp = await fetch("api/chat/" + msg_id, {
+    method: "DELETE",
+  });
 
-    window.setInterval(location.reload(), 500);
+  window.setInterval(location.reload(), 500);
 }
-
---
+++ public/js/registerPet.js
@@ -0,0 +1,16 @@
+async function register() {
+  let postImage = document.querySelector("#image").value;
+  //console.log(postImage);
+  let postAnimal = document.querySelector("#animal").value;
+  let postAge = document.querySelector("#age").value;
+  let postTemp = document.querySelector("$temperment").value;
+  let postColor=document.querySelector("#color");
+  let postSize=document.querySelector("#size");
+  let postBreed=document.querySelector("#breed")
+  let postGender=document.querySelector("#gender")
+  let postHealth=document.querySelector("#health")
+  let postCastrated = document.querySelector("#castrated");
+  let postVacc = document.querySelector("vaccinated");
+
+  
+}
