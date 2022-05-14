69c7719fad43c5af92ec2de1251255be84c5fd8e
Arturo Asis Vazquez Cardenas <aasisvc2000@gmail.com>
2022-05-13 19:56:11 -0500

    chat already completed

--- public/js/chatsActions.js
+++ public/js/chatsActions.js
@@ -1,7 +1,9 @@
 // Funcion para obtener los dos arreglos de los chats del usuario loggeado ya sea owner o guest
-async function getChats(){
+async function getChats(query){
 
-    const response = await fetch('/api/chat', {
+    let queryParams = query ? "?name=" + query : "";
+
+    const response = await fetch('/api/chat' + queryParams, {
         method: 'GET',
         headers: {
             "x-auth": sessionStorage.getItem("token")
@@ -18,17 +20,18 @@ async function chatsToHtml(data){
     let divChats = document.querySelector("ul#chats");
     console.log(divChats);
 
-    divChats.innerHTML = data.ownerChats.map(chat => chatToHTML(chat, true)).join("");
-    divChats.innerHTML += data.guestChats.map(chat => chatToHTML(chat, false)).join("");
+    divChats.innerHTML = data.ownerChats.map(chat => chatToHTMLBanner(chat, true)).join("");
+    divChats.innerHTML += data.guestChats.map(chat => chatToHTMLBanner(chat, false)).join("");
 }
 
 //Funcion que retorna el html que se necesita para que salgan los chats del usuario loggeado
-function chatToHTML(chat, owner){
+function chatToHTMLBanner(chat, owner){
     return `
-    <li class="person ${owner ? 'owner' : 'guest'}" data-chat="person1" onclick="selectChat('${chat._id}', owner)"> 
+    <li class="person ${owner ? 'owner' : 'guest'}" data-chat="person1" onclick="selectChat('${chat._id}', ${owner})"> 
         <div class="user">
             <img src=${chat.idPost.image} alt="Retail Admin">
         </div>
+
         <p class="name-time">
             <span class="name ${owner ? 'owner' : 'guest'}">${chat.idPost.name} ${chat.idPost.animal}</span>
             <br>
@@ -38,82 +41,65 @@ function chatToHTML(chat, owner){
 `
 }
 
+//todo: guardar el chatId, encontrar el chat del que se quiere actuar y mandarlo
+function selectChat(chatId, owner){
+    console.log(chatId);
+    sessionStorage.setItem("chatId", chatId);
+    let selectedChat = "";
+    let chats = JSON.parse(sessionStorage.getItem("chats"));
+
+    if(owner){
+        selectedChat = chats.ownerChats.find(chat => chat._id == chatId);
+    }else{
+        selectedChat = chats.guestChats.find(chat => chat._id == chatId);
+    }
+
+    console.log(selectedChat);
+    let divMensajesChat = document.querySelector("#chatEspecifico");
+    divMensajesChat.innerHTML = specificChatHTML(selectedChat, owner);
+}
+
 //Funcion que identifica el textarea donde el usuario escribiria el mensaje y realiza la actualizacion del chat con el nuevo mensaje
 async function sendNewMessage(){
-    let texTarea = document.querySelector("textarea#inputMessage").value;
-    console.log(texTarea);
 
-    const resp = await fetch('/api/chat' + sessionStorage.getItem("chatId"), {
+    let message = {
+        'msg': document.getElementById("inputMessage").value
+    }
+    console.log(message);
+
+    const resp = await fetch('/api/chat/' + sessionStorage.getItem("chatId"), {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json',
             "x-auth": sessionStorage.getItem("token")
         },
-        body: JSON.stringify(texTarea)
+        body: JSON.stringify(message)
     })
 
     const result = await resp.json();
     console.log(result);
+    window.setInterval(location.reload(), 500);
 }
 
-//Funcion que contiene la forma del mensaje del otro usuario en html
-function msg_left(chat){
-    return `
-    <li class="chat-left">
-        <div class="chat-avatar">
-            <img src="https://www.bootdey.com/img/Content/avatar/avatar3.png" alt="Retail Admin">
-            <div class="chat-name">Russell</div>
-        </div>
-
-        <div class="chat-text">Actually everything was fine.
-            <br>I'm very excited to show this to our team.</div>
-        <div class="chat-hour">07:00 <span class="fa fa-check-circle"></span></div>
-    </li>
-    `
-}
-
-//Funcion que contiene la forma del mensaje del usuario loggeado en html
-function msg_right(chat){
-    return `
-    <li class="chat-right">
-        <div class="chat-hour">08:59 <span class="fa fa-check-circle"></span></div>
-        <div class="chat-text">vamos a ver si este moihrasfhraodhforhfihih hfoadhfsrhfiheri fifdhfi hfoehgriepgh fiensaje se adapta con la nube vamos si jala jala </div>
-        <div class="chat-avatar">
-            <img src="https://www.bootdey.com/img/Content/avatar/avatar4.png" alt="Retail Admin">
-                <div class="chat-name">Jin</div>
-        </div>
-    </li>
-    `
+async function buscarChat(){
+    event.preventDefault();
+    let divBuscador = document.getElementById("busqueda").value;
+    console.log(divBuscador);
+    let data = await getChats(divBuscador);
+    chatsToHtml(data);
 }
 
 //Funcion para organizar las funciones de chat
 async function start(){
     let data = await getChats();
     chatsToHtml(data);
-    localStorage.setItem("chats", JSON.stringify(data));
+    sessionStorage.setItem("chats", JSON.stringify(data));
 }
 
 start();
 
-//todo: guardar el chatId, encontrar el chat del que se quiere actuar y mandarlo
-function selectChat(chatId, owner){
-    console.log(chatId);
-    sessionStorage.setItem("chatId", chatId);
-
-    let data = localStorage.getItem("chats");
-    let chats = JSON.parse(data);
-
-    if(owner){
-        chats.ownerChats.find(chat => chat._id == chatId);
-    }else{
-        chats.guestChats.find(chat)
-    }
-
-}
-
 //Funcion para crear un chat si no existe al hacer clic a send message
 async function processToChat(post_id){
-
     let ownerId = post_id.userID;
     console.log(ownerId);
 
@@ -130,25 +116,75 @@ async function processToChat(post_id){
 }
 
 //Funcion que obtiene todos los usuarios
-
 async function getUsersToFind(ownerId){
+    const response = await fetch('/api/register', {
+        method: 'GET',
+    });
 
+    const data = await response.json();
+
+    let obj = data.find(idOwner => idOwner.id == ownerId);
+    return obj._id;
 }
 
 //Funcion que contiene la esctrucura de los chats con los mensajes (dentro del ul)
-async function specificChatHTML(data){
-    `
+function specificChatHTML(chat, owner){
+    let myId = owner ? chat.idOwner._id : chat.idGuest._id;
+
+    return `
     <div class="selected-user" id="msgTo">
-        <span>To: <span class="name">Emily Russell</span></span>
+        <span><span class="name">${chat.idPost.name} Pet Conversation</span></span>
     </div>
                                 
     <div class="chat-container">
         <ul class="chat-box chatContainerScroll" id="mensajes">
-        
+        ${chat.messages.map(msg => msg_html(msg, msg.idSenderUser == myId ? "right" : "left", msg.idSenderUser == chat.idOwner._id ? chat.idOwner : chat.idGuest)).join('')}
         </ul>
         <div class="form-group mt-3 mb-0">
-            <textarea class="form-control" rows="3" id="inputMessage" placeholder="Type your message here..." onsubmit="sendNewMessage()"></textarea>
+            <textarea class="form-control" rows="3" id="inputMessage" placeholder="Type your message here..."></textarea>
+            <button type="button" class="btn btn-info" onclick="sendNewMessage()">send</button>
         </div>
     </div>
     `
 }
+
+//Funcion que contiene la forma del mensaje del otro usuario en html
+function msg_html(msg, side, user){
+
+    if(side == "left"){
+        return `
+            <li class="chat-${side}">
+                <div class="chat-avatar">
+                    <img src=${user.image} alt="Retail Admin">
+                    <div class="chat-name">${user.name} ${user.lastname}</div>
+                </div>
+
+                <div class="chat-text">${msg.message}</div>
+                <div class="chat-hour">${msg.date} <span class="fa fa-check-circle"></span></div>
+            </li>
+    `
+    }else{
+        return `
+            <li class="chat-${side}">
+            <div class="chat-hour">${msg.date} <span class="fa fa-check-circle"></span></div>
+                <div class="chat-text">${msg.message}</div>
+
+                <div class="chat-avatar">
+                    <img src=${user.image} alt="Retail Admin">
+                    <div class="chat-name">${user.name} ${user.lastname}</div>
+                </div>
+                <button type="button" class="btn btn-outline-info" onclick="deleteMessage(${msg._id})">X</button>
+            </li>
+    `
+    }
+}
+
+async function deleteMessage(msg_id){
+    console.log(msg_id);
+    const resp = await fetch('api/chat/' + msg_id, {
+        method: 'DELETE'
+    })
+
+    window.setInterval(location.reload(), 500);
+}
+
